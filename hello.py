e38ebed33f7876cacfa7d563e541f6945ce5251c
Unknown <rudsonmartina@gmail.com>
2021-12-02 23:28:16 +0100

    Hallo-Python-1.13
    Added my first python program hello.py and the evidence that it worked hello.png

---
+++ hello.py
@@ -0,0 +1,24 @@
+A =  "Hello wereld van…"
+
+B =  "pppp    y     y   ttttttt   h   h    oooo   nnnn "
+C =  "p   p    y   y       t      h   h   o    o  n   n"
+D =  "p   p     y y        t      h   h   o    o  n   n"
+E =  "pppp       y         t      hhhhh   o    o  n   n"
+F =  "p          y         t      h   h   o    o  n   n"
+G =  "p          y         t      h   h   o    o  n   n"
+H =  "p          y         t      h   h    oooo   n   n"
+
+
+print(A)
+print(B)
+print(C)
+print(D)
+print(E)
+print(F)
+print(G)
+print(H)
+
+
+
+#99066434
+#Rudson Martina
